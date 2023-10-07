c251814078357df80c3e797154f2087ef30d5dd2
romakelap <romakelapa833@gmail.com>
2023-10-07 08:16:12 +0800

    update user detail repo

--- src/main/java/GapiV2/Garlands/repository/UserDetailRepo.java
+++ src/main/java/GapiV2/Garlands/repository/UserDetailRepo.java
@@ -2,5 +2,12 @@ package GapiV2.Garlands.repository;
 
 import GapiV2.Garlands.entity.UserDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserDetailRepo extends JpaRepository<UserDetail, Integer> {
+    Optional<UserDetail> findByUser_Id(int id);
+    List<UserDetail> findAllByUser_IdOrderByIdDesc(int userId);
+    void deleteById(int id);
 }
