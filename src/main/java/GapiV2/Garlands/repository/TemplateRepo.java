0493283d49662ed39025b2ea0811c971163b34f4
romakelap <romakelapa833@gmail.com>
2023-10-07 08:14:20 +0800

    Adding TemplateRepo

---
+++ src/main/java/GapiV2/Garlands/repository/TemplateRepo.java
@@ -0,0 +1,7 @@
+package GapiV2.Garlands.repository;
+
+import GapiV2.Garlands.entity.Template;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemplateRepo extends JpaRepository<Template,Integer> {
+}
