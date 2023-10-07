b8905805451e0e46c02e5201456948762b16e169
romakelap <romakelapa833@gmail.com>
2023-10-07 08:09:34 +0800

    update cms Repo

--- src/main/java/GapiV2/Garlands/repository/CmsRepo.java
+++ src/main/java/GapiV2/Garlands/repository/CmsRepo.java
@@ -3,6 +3,11 @@ package GapiV2.Garlands.repository;
 import GapiV2.Garlands.entity.CMS;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CmsRepo extends JpaRepository<CMS, Integer> {
+
+    List<CMS> findAllByUser_IdOrderById(int userId);
+    void deleteById(int id);
 
 }
