a4814a69622532f653e23b8b4037550766de7985
romakelap <romakelapa833@gmail.com>
2023-10-07 08:13:29 +0800

    Adding status sale bill history repo

---
+++ src/main/java/GapiV2/Garlands/repository/StatusSaleBillHistoryRepo.java
@@ -0,0 +1,7 @@
+package GapiV2.Garlands.repository;
+
+import GapiV2.Garlands.entity.StatusSaleBillHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusSaleBillHistoryRepo extends JpaRepository<StatusSaleBillHistory, Integer> {
+}
