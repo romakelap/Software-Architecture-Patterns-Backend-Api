e74e9592a5f4c31be543d55b88dab2289586ce9b
romakelap <romakelapa833@gmail.com>
2023-10-29 11:14:13 +0800

    Update on Cms Controller

--- src/main/java/GapiV2/Garlands/controller/CmsController.java
+++ src/main/java/GapiV2/Garlands/controller/CmsController.java
@@ -1,10 +1,16 @@
 package GapiV2.Garlands.controller;
 
+import GapiV2.Garlands.entity.CMS;
+import GapiV2.Garlands.entity.Template;
+import GapiV2.Garlands.repository.CmsRepo;
 import GapiV2.Garlands.request.CmsAddRequest;
 import GapiV2.Garlands.response.CmsGetResponse;
 import GapiV2.Garlands.service.CmsService;
+import GapiV2.Garlands.service.TemplateService;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,9 +19,13 @@ import java.util.List;
 @RequestMapping("api/cms")
 public class CmsController {
     private final CmsService cmsService;
+    private final CmsRepo cmsRepo;
+    private final TemplateService templateService;
 
-    public CmsController(CmsService cmsService) {
+    public CmsController(CmsService cmsService, CmsRepo cmsRepo, TemplateService templateService) {
         this.cmsService = cmsService;
+        this.cmsRepo = cmsRepo;
+        this.templateService = templateService;
     }
 
     @GetMapping("/get-all")
@@ -25,12 +35,50 @@ public class CmsController {
 
 
     @GetMapping("/get-by-id/{id}")
-    public ResponseEntity<CmsGetResponse> getById(@PathVariable int id){
-        return new ResponseEntity<>(cmsService.getResponseById(id),HttpStatus.OK);
+    public ResponseEntity<String> getById(@PathVariable int id) {
+        CmsGetResponse cmsResponse = cmsService.getResponseById(id);
+        if (cmsResponse != null) {
+            int templateId = cmsResponse.getTemplateId();
+            Template template = templateService.getWebsiteById(templateId);
+
+            if (template != null) {
+                String websiteContent = generateWebsiteContent(template, cmsResponse);
+                return ResponseEntity.ok().body(websiteContent);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private String generateWebsiteContent(Template template, CmsGetResponse cmsResponse) {
+        String htmlContent = template.getHtmlContent();
+        String cssContent = template.getCssContent();
+        String jsContent = template.getJsContent();
+
+        // Ganti placeholder dengan data dari CMS
+        htmlContent = htmlContent.replace("{{title}}", cmsResponse.getTitle());
+        htmlContent = htmlContent.replace("{{content}}", cmsResponse.getContent());
+        htmlContent = htmlContent.replace("{{location}}", cmsResponse.getLocation());
+        htmlContent = htmlContent.replace("{{userFirstName}}", cmsResponse.getUserFirstName());
+        htmlContent = htmlContent.replace("{{userLastName}}", cmsResponse.getUserLastName());
+
+        // Menggabungkan konten HTML, CSS, dan JS dari template
+        String websiteContent = "<html><head><style>" +
+                cssContent +
+                "</style></head><body>" +
+                htmlContent +
+                "<script>" +
+                jsContent +
+                "</script></body></html>";
+
+        return websiteContent;
     }
 
 
-    @GetMapping("/get-all-by-user/{userId}")
+
+    @GetMapping("/get-by-user/{userId}")
     public ResponseEntity<List<CmsGetResponse>> getAllByUser(@PathVariable int userId){
         return new ResponseEntity<>(cmsService.getAllByUser(userId),HttpStatus.OK);
     }
@@ -49,5 +97,10 @@ public class CmsController {
         return new ResponseEntity<>("Deleted",HttpStatus.OK);
     }
 
-
+    @GetMapping("/cms/{id}")
+    public CMS getCMS(@PathVariable int id, Model model) throws ChangeSetPersister.NotFoundException {
+        CMS cms = cmsRepo.findById(id).orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+        model.addAttribute("cms", cms);
+        return cms;
+    }
 }
